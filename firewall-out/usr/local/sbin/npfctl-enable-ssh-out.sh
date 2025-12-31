2c1ea214f0eafb99478b4739b287ba77abf6111a
Henryk Paluch <hpaluch@seznam.cz>
2025-12-31 12:38:36 +0000

    really enable ssh (was copy&paste http(s))

--- firewall-out/usr/local/sbin/npfctl-enable-ssh-out.sh
+++ firewall-out/usr/local/sbin/npfctl-enable-ssh-out.sh
@@ -2,7 +2,6 @@
 set -xeuo pipefail
 sd=
 [ `id -u` -eq 0 ] || sd=doas
-$sd npfctl rule "dyn-wired" add pass stateful out final proto tcp flags S/FSRA to any port http
-$sd npfctl rule "dyn-wired" add pass stateful out final proto tcp flags S/FSRA to any port https
+$sd npfctl rule "dyn-wired" add pass stateful out final proto tcp flags S/FSRA to any port ssh
 $sd npfctl rule dyn-wired list
 exit 0
